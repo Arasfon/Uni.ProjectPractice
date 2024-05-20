ff2584f42758630837e6e73345a07009b9c8c6f9
Egor Boyarkin <egrsfn@gmail.com>
2024-05-21 01:05:10 +0300

    Add clarifying startup message in task 6

--- 4th semester/ProjectPractice/projectpractice/task6/main.py
+++ 4th semester/ProjectPractice/projectpractice/task6/main.py
@@ -1,6 +1,8 @@
 from functions import average_grade_in_the_south_district, count_students_in_the_north_east_district_with_informatics_as_beloved_lesson
 from input import read_students_from_stdin
 
+print('Данная программа считает количество учеников в Северо-Восточном округе с любимым предметом "математика", '
+      + 'а также Средний балл учеников в Южном округе.')
 
 student_count = int(input("Введите количество учеников: "))
 students = read_students_from_stdin(student_count)
